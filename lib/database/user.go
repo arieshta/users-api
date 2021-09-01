1dd72f134902183026e15589a4a000aa167a568a
Ilham Aris Hartadi <44136041+arieshta@users.noreply.github.com>
2021-09-01 15:23:39 +0700

    add CreateUser feature

--- lib/database/user.go
+++ lib/database/user.go
@@ -1,1 +1,12 @@
 package database
+
+import (
+	"users-api/models"
+)
+
+func CreateUser(user *models.Users) error {
+	if err := config.DB.Table("users").Create(&user).Error; err != nil {
+		return err
+	}
+	return nil
+}
